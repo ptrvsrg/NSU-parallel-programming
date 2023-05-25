e35145ff688b570c719722ccc670b7ab00729ef1
ptrvsrg <s.petrov1@g.nsu.ru>
2023-05-25 18:14:59 +0700

    Add task queue implementation

---
+++ lab5/include/task_queue.h
@@ -0,0 +1,24 @@
+#ifndef TASK_QUEUE_H
+#define TASK_QUEUE_H
+
+#include <stdbool.h>
+
+#define SUCCESS 0
+#define ERROR (-1)
+
+struct task_t {
+    int id;
+    int process_id;
+    int weight;
+};
+
+struct task_queue_t;
+
+struct task_queue_t *task_queue_create(int capacity);
+bool task_queue_is_empty(const struct task_queue_t *queue);
+bool task_queue_is_full(const struct task_queue_t *queue);
+int task_queue_push(struct task_queue_t *queue, struct task_t task);
+int task_queue_pop(struct task_queue_t *queue, struct task_t *task);
+void task_queue_destroy(struct task_queue_t **queue);
+
+#endif // TASK_QUEUE_H
---
+++ lab5/src/task_queue.c
@@ -0,0 +1,83 @@
+#include "task_queue.h"
+#include <stdlib.h>
+
+struct task_queue_t {
+    struct task_t *data;
+    int capacity;
+    int count;
+    int pop_index;
+};
+
+struct task_queue_t *task_queue_create(int capacity) {
+    struct task_queue_t *queue = malloc(sizeof(struct task_queue_t));
+    if (queue == NULL) {
+        return NULL;
+    }
+
+    struct task_t *data = malloc(sizeof(struct task_t) * capacity);
+    if (data == NULL) {
+        return NULL;
+    }
+
+    queue->data = data;
+    queue->capacity = capacity;
+    queue->count = 0;
+    queue->pop_index = 0;
+
+    return queue;
+}
+
+bool task_queue_is_empty(const struct task_queue_t *queue) {
+    return queue->count == 0;
+}
+
+bool task_queue_is_full(const struct task_queue_t *queue) {
+    return queue->count == queue->capacity;
+}
+
+int task_queue_push(struct task_queue_t *queue, struct task_t task) {
+    if (queue == NULL) {
+        return ERROR;
+    }
+
+    if (task_queue_is_full(queue)) {
+        return ERROR;
+    }
+
+    int push_index = (queue->pop_index + queue->count) % queue->capacity;
+    queue->data[push_index] = task;
+    queue->count++;
+
+    return SUCCESS;
+}
+
+int task_queue_pop(struct task_queue_t *queue, struct task_t *task) {
+    if (queue == NULL) {
+        return ERROR;
+    }
+
+    if (task_queue_is_empty(queue)) {
+        return ERROR;
+    }
+
+    *task = queue->data[queue->pop_index];
+    queue->pop_index = (queue->pop_index + 1) % queue->capacity;
+    queue->count--;
+
+    return SUCCESS;
+}
+
+void task_queue_destroy(struct task_queue_t **queue) {
+    if (*queue == NULL) {
+        return;
+    }
+
+    if ((*queue)->data == NULL) {
+        return;
+    }
+
+    free((*queue)->data);
+    free(*queue);
+
+    *queue = NULL;
+}
