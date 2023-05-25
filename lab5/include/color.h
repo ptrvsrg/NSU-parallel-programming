70ac09daf057e45a62eb5280eee32bb7ffcbec48
ptrvsrg <s.petrov1@g.nsu.ru>
2023-05-25 21:08:06 +0700

    Add new color

--- lab5/include/color.h
+++ lab5/include/color.h
@@ -2,10 +2,13 @@
 #define COLOR_H
 
 #define FNORM       "\x1B[0m"
+#define FBLACK      "\x1B[30m"
 #define FRED        "\x1B[31m"
 #define FGREEN      "\x1B[32m"
 #define FYELLOW     "\x1B[33m"
 #define FBLUE       "\x1B[34m"
 #define FMAGENTA    "\x1B[35m"
+#define FCYAN       "\x1B[36m"
+#define FWHITE      "\x1B[37m"
 
 #endif // COLOR_H
