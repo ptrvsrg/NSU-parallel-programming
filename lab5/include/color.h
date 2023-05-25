16ffa97442efad0cd8dc4f1e4ecd5e28c6256478
ptrvsrg <s.petrov1@g.nsu.ru>
2023-05-25 18:15:12 +0700

    Add colors for printf

---
+++ lab5/include/color.h
@@ -0,0 +1,11 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define FNORM       "\x1B[0m"
+#define FRED        "\x1B[31m"
+#define FGREEN      "\x1B[32m"
+#define FYELLOW     "\x1B[33m"
+#define FBLUE       "\x1B[34m"
+#define FMAGENTA    "\x1B[35m"
+
+#endif // COLOR_H
